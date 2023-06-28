279dc721c7e968e73b0a1a4a47f750274cd52423
AlonLavy <alon.lavy2@gmail.com>
2023-06-28 14:28:01 +0300

    board created

--- src/App.js
+++ src/App.js
@@ -1,3 +1,37 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
+
+function Square() {
+  const [value, setValue] = useState(null);
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+
+  function handleClick() {
+    setValue("X");
+  }
 }
